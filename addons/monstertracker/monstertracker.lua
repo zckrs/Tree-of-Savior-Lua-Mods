87976f0a2c776461c90be40e2933259fed2bb50a
Excrulon <excrulon@gmail.com>
2016-04-13 00:25:10 -0400

    fixed an issue with monster tracker where it didn't trigger for everyone (such as a heal object or other spells killing a mob). hooked it to the generic fps loop instead since the mob dead event isn't reliable.

--- addons/monstertracker/monstertracker.lua
+++ addons/monstertracker/monstertracker.lua
@@ -1,52 +1,64 @@
 _G["MONSTER_TRACKER"] = {};
 
-function MON_DEAD_CLIENT_HOOKED(actor)
-	_G["MON_DEAD_CLIENT_OLD"](actor);
+function FPS_ON_MSG_HOOKED(frame, msg, argStr, argNum)
+	_G["FPS_ON_MSG_OLD"](frame, msg, argStr, argNum);
 
-	local obj = actor;
-	local monID = obj:GetType();
-	if monID ~= 0 then
-		local monCls = GetClassByType("Monster", monID);
+	local result = SCR_GET_ZONE_FACTION_OBJECT(session.GetMapName(), "Monster", "Normal/Material/Elite", 120000);
 
+	for k,v in pairs(result) do
+	    local mobName = v[1];
+	    local totalSpawn = v[2];
+
+		local monCls = GetClass("Monster", mobName);
 		local wiki = GetWikiByName(monCls.Journal);
+
 		local killCount = GetWikiIntProp(wiki, "KillCount") + 1;
 		local killsRequired = GetClass('Journal_monkill_reward', monCls.Journal).Count1;
+
+		if _G["MONSTER_TRACKER"][monCls.ClassID] == nil then
+			_G["MONSTER_TRACKER"][monCls.ClassID] = killCount;
+		end
+
+		if killCount > _G["MONSTER_TRACKER"][monCls.ClassID] then
+
+			local killNoticeFrame = ui.GetFrame("killnotice");
+			killNoticeFrame:SetGravity(ui.RIGHT, ui.BOTTOM);
+
+			local monsterImage = GET_CHILD(killNoticeFrame, "monsterImage");
+			monsterImage:SetImage(GET_MON_ILLUST(monCls));
+			monsterImage:SetGravity(ui.LEFT, ui.TOP);
+			monsterImage:Resize(50, 50);
+			monsterImage:Move(0, 0);
+			monsterImage:SetOffset(0, 0);
+
+			local killNoticeText = killNoticeFrame:GetChild("killNoticeText");
+			tolua.cast(killNoticeText, "ui::CRichText");
+			killNoticeText:SetText("{@st42}" .. ADD_THOUSANDS_SEPARATOR(killCount) .. " / " .. ADD_THOUSANDS_SEPARATOR(killsRequired) .. " " .. monCls.Name .. " killed{/}");
+			killNoticeText:SetGravity(ui.LEFT, ui.TOP);
+			killNoticeText:SetTextAlign("left", "top");
+			killNoticeText:Move(0, 0);
+			killNoticeText:SetOffset(55, 17);
+
+			killNoticeFrame:ShowWindow(1);
+			killNoticeFrame:SetDuration(5.0);
+		end
+
+		_G["MONSTER_TRACKER"][monCls.ClassID] = killCount;
 
 		if killCount == killsRequired then
 			ui.SysMsg("Congrats! You've completed the monster kill requirements for " .. monCls.Name .. "! Redeem your reward at Wings of Vibora in town!");
 			imcSound.PlaySoundEvent("sys_levelup");
 		end
-
-		local killNoticeFrame = ui.GetFrame("killnotice");
-		killNoticeFrame:SetGravity(ui.RIGHT, ui.BOTTOM);
-
-		local monsterImage = GET_CHILD(killNoticeFrame, "monsterImage");
-		monsterImage:SetImage(GET_MON_ILLUST(monCls));
-		monsterImage:SetGravity(ui.LEFT, ui.TOP);
-		monsterImage:Resize(50, 50);
-		monsterImage:Move(0, 0);
-		monsterImage:SetOffset(0, 0);
-
-		local killNoticeText = killNoticeFrame:GetChild("killNoticeText");
-		tolua.cast(killNoticeText, "ui::CRichText");
-		killNoticeText:SetText("{@st42}" .. ADD_THOUSANDS_SEPARATOR(killCount) .. " / " .. ADD_THOUSANDS_SEPARATOR(killsRequired) .. " " .. monCls.Name .. " killed{/}");
-		killNoticeText:SetGravity(ui.LEFT, ui.TOP);
-		killNoticeText:SetTextAlign("left", "top");
-		killNoticeText:Move(0, 0);
-		killNoticeText:SetOffset(55, 17);
-
-		killNoticeFrame:ShowWindow(1);
-		killNoticeFrame:SetDuration(5.0);
 	end
 end
 
-local deadMobHook = "MON_DEAD_CLIENT";
+local FPS_ON_MSGHook = "FPS_ON_MSG";
 
-if _G["MON_DEAD_CLIENT_OLD"] == nil then
-	_G["MON_DEAD_CLIENT_OLD"] = _G[deadMobHook];
-	_G[deadMobHook] = MON_DEAD_CLIENT_HOOKED;
+if _G["FPS_ON_MSG_OLD"] == nil then
+	_G["FPS_ON_MSG_OLD"] = _G[FPS_ON_MSGHook];
+	_G[FPS_ON_MSGHook] = FPS_ON_MSG_HOOKED;
 else
-	_G[deadMobHook] = MON_DEAD_CLIENT_HOOKED;
+	_G[FPS_ON_MSGHook] = FPS_ON_MSG_HOOKED;
 end
 
 ui.SysMsg("Monster Tracker loaded!");
