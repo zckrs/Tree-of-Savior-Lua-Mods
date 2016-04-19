17de8cfaff2f5d71c33238d1b7d9c8e76c952ce0
Excrulon <excrulon@gmail.com>
2016-04-18 23:03:09 -0400

    Fixed reward alert so that it only shows up once. Fixed issue with looping reward alert when landing on the number of kills required.

--- addons/monstertracker/monstertracker.lua
+++ addons/monstertracker/monstertracker.lua
@@ -5,26 +5,58 @@ _G["MONSTER_TRACKER"]["settings"] = {
 	showNoticeIfComplete = true;
 };
 
+local MonsterTrackData = {}
+MonsterTrackData.__index = MonsterTrackData
+
+setmetatable(MonsterTrackData, {
+	__call = function (cls, ...)
+		return cls.new(...)
+	end,
+})
+
+function MonsterTrackData.new()
+	local self = setmetatable({}, MonsterTrackData)
+
+	self.previousKillCount = 0;
+	self.killCount = 0;
+	self.killsRequired = 0;
+	self.needsToShow = false;
+	self.isCompleted = false;
+	self.isFirstPass = true;
+
+	return self
+end
+
 function FPS_ON_MSG_HOOKED(frame, msg, argStr, argNum)
 	_G["FPS_ON_MSG_OLD"](frame, msg, argStr, argNum);
 
-	local result = SCR_GET_ZONE_FACTION_OBJECT(session.GetMapName(), "Monster", "Normal/Material/Elite", 120000);
+	local result = SCR_GET_ZONE_FACTION_OBJECT(session.GetMapName(), "Monster", "Normal/Material/Elite/Boss", 120000);
 
 	for k,v in pairs(result) do
 	    local mobName = v[1];
 	    local totalSpawn = v[2];
-		
+
 		local monCls = GetClass("Monster", mobName);
 		local wiki = GetWikiByName(monCls.Journal);
-		local killCount = GetWikiIntProp(wiki, "KillCount") + 1;
-		local killsRequired = GetClass('Journal_monkill_reward', monCls.Journal).Count1;
 
-		if _G["MONSTER_TRACKER"][monCls.ClassID] == nil then
-			_G["MONSTER_TRACKER"][monCls.ClassID] = killCount;
+		local monsterTrackData = _G["MONSTER_TRACKER"][monCls.ClassID];
+
+		if monsterTrackData == nil then
+			monsterTrackData = MonsterTrackData();
 		end
 
-		if killCount > _G["MONSTER_TRACKER"][monCls.ClassID] then
+		monsterTrackData.killCount = GetWikiIntProp(wiki, "KillCount");
+		monsterTrackData.killsRequired = GetClass('Journal_monkill_reward', monCls.Journal).Count1;
 
+		if monsterTrackData.isFirstPass then
+			monsterTrackData.previousKillCount = monsterTrackData.killCount;
+
+			if monsterTrackData.killCount >= monsterTrackData.killsRequired then
+				monsterTrackData.isCompleted = true;
+			end
+		end
+
+		if monsterTrackData.killCount ~= monsterTrackData.previousKillCount then
 			local killNoticeFrame = ui.GetFrame("killnotice");
 			killNoticeFrame:SetGravity(ui.RIGHT, ui.BOTTOM);
 
@@ -37,25 +69,36 @@ function FPS_ON_MSG_HOOKED(frame, msg, argStr, argNum)
 
 			local killNoticeText = killNoticeFrame:GetChild("killNoticeText");
 			tolua.cast(killNoticeText, "ui::CRichText");
-			killNoticeText:SetText("{@st42}" .. ADD_THOUSANDS_SEPARATOR(killCount) .. " / " .. ADD_THOUSANDS_SEPARATOR(killsRequired) .. " " .. monCls.Name .. " killed{/}");
+			killNoticeText:SetText("{@st42}" .. ADD_THOUSANDS_SEPARATOR(monsterTrackData.killCount) .. " / " .. ADD_THOUSANDS_SEPARATOR(monsterTrackData.killsRequired) .. " " .. monCls.Name .. " killed{/}");
 			killNoticeText:SetGravity(ui.LEFT, ui.TOP);
 			killNoticeText:SetTextAlign("left", "top");
 			killNoticeText:Move(0, 0);
 			killNoticeText:SetOffset(55, 17);
 
-			if killCount <= killsRequired or (killCount > killsRequired and _G["MONSTER_TRACKER"]["settings"].showNoticeIfComplete) then
+			if SHOULD_SHOW_NOTICE(monsterTrackData) then
 				killNoticeFrame:ShowWindow(1);
 				killNoticeFrame:SetDuration(5.0);
+				monsterTrackData.needsToShow = false;
 			end
 		end
 
-		_G["MONSTER_TRACKER"][monCls.ClassID] = killCount;
-
-		if killCount == killsRequired then
-			ui.SysMsg("Congrats! You've completed the monster kill requirements for " .. monCls.Name .. "! Redeem your reward at Wings of Vibora in town!");
+		if monsterTrackData.killCount >= monsterTrackData.killsRequired and not monsterTrackData.isFirstPass and not monsterTrackData.isCompleted then
+			ui.SysMsg("Congratulations! You've completed the monster kill requirements for " .. monCls.Name .. "! Redeem your reward at Wings of Vibora in town!");
 			imcSound.PlaySoundEvent("sys_levelup");
+			monsterTrackData.isCompleted = true;
 		end
+
+		monsterTrackData.previousKillCount = monsterTrackData.killCount;
+		monsterTrackData.isFirstPass = false;
+
+		_G["MONSTER_TRACKER"][monCls.ClassID] = monsterTrackData;
 	end
+end
+
+function SHOULD_SHOW_NOTICE(monsterTrackData)
+	return monsterTrackData.killCount <= monsterTrackData.killsRequired
+		and not monsterTrackData.isFirstPass
+		or (monsterTrackData.killCount > monsterTrackData.killsRequired and _G["MONSTER_TRACKER"]["settings"].showNoticeIfComplete);
 end
 
 local FPS_ON_MSGHook = "FPS_ON_MSG";
