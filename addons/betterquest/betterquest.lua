046282b212f6e35c74dc39f05743d49b3dad0972
Mehdy Dara <mdara@eleven-labs.com>
2016-04-25 14:30:10 +0200

    Update to @rncoll7 code
    * Adds a checkbox to the quest UI that will force all eligible quests to be shown
    * Quests are sorted by level
    * Quest level before name

--- addons/betterquest/betterquest.lua
+++ addons/betterquest/betterquest.lua
@@ -1,6 +1,6 @@
 local filterEnabled = true;
 
-function BETTERQUEST_CREATE_FILTER_CHECKBOX()
+function INIT(addon, frame)
 	local frame = ui.GetFrame('quest');
 	local ctrl = frame:CreateOrGetControl('checkbox', 'BETTERQUEST_FILTER', 0, 0, 150, 30);
 	tolua.cast(ctrl, 'ui::CCheckBox');
@@ -11,11 +11,11 @@ function BETTERQUEST_CREATE_FILTER_CHECKBOX()
 	ctrl:SetOverSound('button_over');
 	ctrl:SetEventScript(ui.LBUTTONUP, 'BETTERQUEST_TOGGLE_FILTER');
 	ctrl:SetCheck(filterEnabled == true and 0 or 1);
-end
 
-function BETTERQUEST_ON_INIT()
-	BETTERQUEST_CREATE_FILTER_CHECKBOX();
+	_G['OLD_POSSIBLE_UI_OPEN_CHECK'] = _G['SCR_POSSIBLE_UI_OPEN_CHECK'];
 	_G['SCR_POSSIBLE_UI_OPEN_CHECK'] = BETTERQUEST_POSSIBLE_UI_OPEN_CHECK;
+	_G['OLD_UPDATE_ALLQUEST'] = _G['UPDATE_ALLQUEST'];
+	_G['UPDATE_ALLQUEST'] = BETTERQUEST_UPDATE_ALLQUEST;
 end
 
 local refreshQuestFrame = function()
@@ -68,20 +68,92 @@ function BETTERQUEST_POSSIBLE_UI_OPEN_CHECK(pc, questIES)
 	return 'HIDE';
 end
 
-BETTERQUEST_ON_INIT();
 
-function QUEST_ON_INIT_HOOKED(addon, frame)
-    _G["QUEST_ON_INIT_OLD"](addon, frame);
-    BETTERQUEST_ON_INIT();
+
+function BETTERQUEST_UPDATE_ALLQUEST(frame, msg, isNew, questID, isNewQuest)
+	local pc = GetMyPCObject();
+	local mylevel = info.GetLevel(session.GetMyHandle());
+	local posY = 60;
+
+	local sobjIES = GET_MAIN_SOBJ();
+	local questGbox = frame:GetChild('questGbox');
+
+	local newCtrlAdded = false;
+	if questID ~= nil and questID > 0 then
+		local questIES = GetClassByType("QuestProgressCheck", questID);
+		local result = SCR_QUEST_CHECK_C(pc, questIES.ClassName);
+		local ctrlName = "_Q_" .. questIES.ClassID;
+
+
+		-- ???? ????? ?????????? ???? ?????????? ui???? ???? ????.
+		if isNewQuest == 0 and isNew == 1 then
+			questGbox:RemoveChild(ctrlName);
+		elseif QUEST_ABANDON_RESTARTLIST_CHECK(questIES, sobjIES) == 'NOTABANDON' then
+			local newY = SET_QUEST_LIST_SET(frame, questGbox, posY, ctrlName, questIES, result, isNew, questID);
+			if newY ~= posY then
+				newCtrlAdded = true;
+			end
+			posY = newY;
+		end
+
+	else
+		-- Update All
+		local clsList, cnt = GetClassList("QuestProgressCheck");
+
+		--Sort Init
+		local quests = {}
+		for i = 0, cnt -1 do
+			quests[i] = GetClassByIndexFromList(clsList, i);
+		end
+		table.sort(quests,questSort);
+		--Sort End
+
+		for i = 0, cnt -1 do
+			--local questIES = GetClassByIndexFromList(clsList, i);
+			local questIES = quests[i]; --
+			local questAutoIES = GetClass('QuestProgressCheck_Auto',questIES.ClassName)
+
+			if string.sub(questIES.Name, 1, 1) ~= "[" then
+				questIES.Name = "[" .. questIES.Level .. "] " .. questIES.Name;
+			end
+
+			if questIES.ClassName ~= "None" then
+				local ctrlName = "_Q_" .. questIES.ClassID;
+				local abandonCheck = QUEST_ABANDON_RESTARTLIST_CHECK(questIES, sobjIES)
+				if abandonCheck == 'NOTABANDON' or abandonCheck == 'ABANDON/NOTLIST' then
+
+					local result = SCR_QUEST_CHECK_C(pc, questIES.ClassName);
+					if IS_ABOUT_JOB(questIES) == true then
+						if result ~= 'IMPOSSIBLE' and result ~= 'None' then
+							posY = SET_QUEST_LIST_SET(frame, questGbox, posY, ctrlName, questIES, result, isNew, questID);
+						end
+					else
+						posY = SET_QUEST_LIST_SET(frame, questGbox, posY, ctrlName, questIES, result, isNew, questID);
+					end
+				else
+					questGbox:RemoveChild(ctrlName);
+				end
+			end
+		end
+	end
+
+	ALIGN_QUEST_CTRLS(questGbox);
+	if isNewQuest == nil then
+		UPDATE_QUEST_DETAIL(frame, questID);
+	elseif questID ~= nil and isNewQuest > 0 then
+		local questIES = GetClassByType("QuestProgressCheck", questID);
+		if newCtrlAdded == true then
+			UPDATE_QUEST_DETAIL(frame, questID);
+		end
+	end
+
+	frame:Invalidate();
 end
 
-local questOnInitHook = "QUEST_ON_INIT";
 
-if _G["QUEST_ON_INIT_OLD"] == nil then
-    _G["QUEST_ON_INIT_OLD"] = _G[questOnInitHook];
-    _G[questOnInitHook] = QUEST_ON_INIT_HOOKED;
-else
-    _G[questOnInitHook] = QUEST_ON_INIT_HOOKED;
+INIT();
+ui.SysMsg("BetterQuest loaded!");
+
+function questSort(a, b)
+	return a.Level < b.Level
 end
-
-ui.SysMsg("Better Quest loaded!");
