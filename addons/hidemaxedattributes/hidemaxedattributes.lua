6b98fb4529f6662dfa9acdf195e685bf75371152
TehSeph <kazuhiroichi@gmail.com>
2016-04-29 07:26:44 -0500

    hide already max attributes when "show only attributes that can be learned" is ticked

---
+++ addons/hidemaxedattributes/hidemaxedattributes.lua
@@ -0,0 +1,31 @@
+function MAKE_ABILITYSHOP_ICON_HOOKED(frame, pc, grid, abilityClass, groupClass, posY)
+
+	local onlyShowLearnable = GET_CHILD_RECURSIVELY(frame, "onlyShowLearnable")
+	local abilityLevel = 0;
+	local isMax = false;
+
+	local ability = GetAbilityIESObject(pc, abilityClass.ClassName);
+	if ability ~= nil then
+		abilityLevel = ability.Level;
+	end
+
+	local maxLevel = tonumber(groupClass.MaxLevel)
+	if abilityLevel >= maxLevel then
+		isMax = true;
+	end
+
+	if onlyShowLearnable:IsChecked() == 1 then
+
+		if isMax == true then
+			return posY
+		end
+
+	end
+
+	return _G["MAKE_ABILITYSHOP_ICON_OLD"](frame, pc, grid, abilityClass, groupClass, posY);
+
+end
+
+SETUP_HOOK(MAKE_ABILITYSHOP_ICON_HOOKED, "MAKE_ABILITYSHOP_ICON");
+
+ui.SysMsg("Hide Max Level Attributes loaded!");
