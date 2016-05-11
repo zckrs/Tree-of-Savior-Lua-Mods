34cf49bc466be4429c7d47360649292dc9fb1997
Miei <miei1@outlook.com>
2016-05-11 06:13:45 +0100

    add "Remove Message", situational options for friends (e.g unblock, remove friend) (#91)

--- addons/blockandreport/blockandreport.lua
+++ addons/blockandreport/blockandreport.lua
@@ -25,20 +25,56 @@ function CHAT_RBTN_POPUP_HOOKED(frame, chatCtrl)
 	if targetName == GETMYFAMILYNAME() then
 		return;
 	end
-
+	
+	local friendsListType = session.friends.GetFriendListTypeByFamilyName(targetName);
+	
+	local groupBoxName = chatCtrl:GetParent():GetName();
+	local chatCtrlName = chatCtrl:GetName();
+	
 	local context = ui.CreateContextMenu("CONTEXT_CHAT_RBTN", targetName, 0, 0, 170, 100);
 
 	ui.AddContextMenuItem(context, "Whisper", string.format("ui.WhisperTo('%s')", targetName));
-	ui.AddContextMenuItem(context, "Friend Request", string.format("friends.RequestRegister('%s')", targetName));
-	ui.AddContextMenuItem(context, "Party Request", string.format("PARTY_INVITE('%s')", targetName));
 	ui.AddContextMenuItem(context, "Character Info", string.format("OPEN_PARTY_MEMBER_INFO('%s')", targetName));
-	ui.AddContextMenuItem(context, "Report Bot & Block", string.format("BLOCK_AND_REPORT('%s')", targetName));
-	ui.AddContextMenuItem(context, "Report Bot", string.format("REPORT_AUTOBOT_MSGBOX('%s')", targetName));
-	ui.AddContextMenuItem(context, "Block", string.format("CHAT_BLOCK_MSG('%s')", targetName));
-
+	ui.AddContextMenuItem(context, "Party Request", string.format("PARTY_INVITE('%s')", targetName));
+	
+	if AM_I_LEADER(PARTY_GUILD) == 1 then
+		ui.AddContextMenuItem(context, ClMsg("GUILD_INVITE"), string.format("GUILD_INVITE('%s')", targetName));
+	end
+	
+	if FRIEND_LIST_COMPLETE == friendsListType then
+		ui.AddContextMenuItem(context, "Remove Friend", string.format("FRIEND_REMOVE('%s', 'friends list')", targetName));
+		ui.AddContextMenuItem(context, "Remove Message", string.format("REMOVE_CHAT_CLUSTER('%s', '%s')", chatCtrlName, groupBoxName));
+		ui.AddContextMenuItem(context, "Block", string.format("CHAT_BLOCK_MSG('%s')", targetName));
+	elseif FRIEND_LIST_BLOCKED == friendsListType then
+		ui.AddContextMenuItem(context, "Remove Message", string.format("REMOVE_CHAT_CLUSTER('%s', '%s')", chatCtrlName, groupBoxName));
+		ui.AddContextMenuItem(context, "Report Bot", string.format("REPORT_AUTOBOT_MSGBOX('%s')", targetName));
+		ui.AddContextMenuItem(context, "Unblock", string.format("FRIEND_REMOVE('%s', 'block list')", targetName));
+	else
+		ui.AddContextMenuItem(context, "Friend Request", string.format("friends.RequestRegister('%s')", targetName));
+		ui.AddContextMenuItem(context, "Remove Message", string.format("REMOVE_CHAT_CLUSTER('%s', '%s')", chatCtrlName, groupBoxName));
+		ui.AddContextMenuItem(context, "Report Bot & Block", string.format("BLOCK_AND_REPORT('%s')", targetName));
+		ui.AddContextMenuItem(context, "Report Bot", string.format("REPORT_AUTOBOT_MSGBOX('%s')", targetName));
+		ui.AddContextMenuItem(context, "Block", string.format("CHAT_BLOCK_MSG('%s')", targetName));
+	end
+	
 	ui.AddContextMenuItem(context, "Cancel", "None");
 	ui.OpenContextMenu(context);
 
+end
+
+function FRIEND_REMOVE(targetName, listname)
+	local friendsListType = session.friends.GetFriendListTypeByFamilyName(targetName);
+	local cnt = session.friends.GetFriendCount(friendsListType);
+	
+	for i = 0 , cnt - 1 do
+		local f = session.friends.GetFriendByIndex(friendsListType, i);
+		if targetName == f:GetInfo():GetFamilyName() then
+			local msgBoxString = string.format("Do you want to remove %s from your %s?", targetName, listname);
+			local unblockScp = string.format("friends.RequestDelete('%s')", f:GetInfo():GetACCID());
+			ui.MsgBox(msgBoxString, unblockScp, "None");
+			return;
+		end
+	end
 end
 
 function OPEN_PARTY_MEMBER_INFO_HOOKED(targetName)
@@ -69,6 +105,8 @@ function SHOW_PC_CONTEXT_MENU_HOOKED(handle)
 	end
 
 	if pcObj:IsMyPC() == 0 and info.IsPC(pcObj:GetHandleVal()) == 1 then
+	
+		local friendsListType = session.friends.GetFriendListTypeByFamilyName(targetName);
 
 		local context = ui.CreateContextMenu("PC_CONTEXT_MENU", targetName, 0, 0, 170, 100);
 
@@ -77,7 +115,14 @@ function SHOW_PC_CONTEXT_MENU_HOOKED(handle)
 		if session.world.IsIntegrateServer() == false then
 
 			ui.AddContextMenuItem(context, "Whisper", string.format("ui.WhisperTo('%s')", targetName));
+			ui.AddContextMenuItem(context, "Character Info", string.format("PROPERTY_COMPARE(%d)", handle));
 			ui.AddContextMenuItem(context, "Party Request", string.format("PARTY_INVITE('%s')", targetName));
+			
+			if FRIEND_LIST_COMPLETE == friendsListType then
+				ui.AddContextMenuItem(context, "Remove Friend", string.format("FRIEND_REMOVE('%s', 'friends list')", targetName));
+			else
+				ui.AddContextMenuItem(context, "Friend Request", string.format("friends.RequestRegister('%s')", targetName));
+			end
 
 			if AM_I_LEADER(PARTY_GUILD) == 1 then
 				ui.AddContextMenuItem(context, ClMsg("GUILD_INVITE"), string.format("GUILD_INVITE('%s')", targetName));
@@ -87,16 +132,14 @@ function SHOW_PC_CONTEXT_MENU_HOOKED(handle)
 
 		end
 
-		ui.AddContextMenuItem(context, "Character Info", string.format("PROPERTY_COMPARE(%d)", handle));
-
-		if session.world.IsIntegrateServer() == false then
-			ui.AddContextMenuItem(context, "Friend Request", string.format("friends.RequestRegister('%s')", targetName));
+		if FRIEND_LIST_BLOCKED == friendsListType then
+			ui.AddContextMenuItem(context, "Report Bot", string.format("REPORT_AUTOBOT_MSGBOX('%s')", targetName));
+			ui.AddContextMenuItem(context, "Unblock", string.format("FRIEND_REMOVE('%s', 'block list')", targetName));
+		else
+			ui.AddContextMenuItem(context, "Report Bot & Block", string.format("BLOCK_AND_REPORT('%s')", targetName));
+			ui.AddContextMenuItem(context, "Report Bot", string.format("REPORT_AUTOBOT_MSGBOX('%s')", targetName));
+			ui.AddContextMenuItem(context, "Block", string.format("CHAT_BLOCK_MSG('%s')", targetName));
 		end
-
-		ui.AddContextMenuItem(context, "Friendly Duel Request", string.format("REQUEST_FIGHT(%d)", pcObj:GetHandleVal()));
-		ui.AddContextMenuItem(context, "Report Bot & Block", string.format("BLOCK_AND_REPORT('%s')", targetName));
-		ui.AddContextMenuItem(context, "Report Bot", string.format("REPORT_AUTOBOT_MSGBOX('%s')", targetName));
-		ui.AddContextMenuItem(context, "Block", string.format("CHAT_BLOCK_MSG('%s')", targetName));
 		if session.world.IsIntegrateServer() == false then
 			if session.likeit.AmILikeYou(targetName) == true then
 				ui.AddContextMenuItem(context, "Cancel Like", string.format("SEND_PC_INFO(%d)", handle));
@@ -114,7 +157,41 @@ function SHOW_PC_CONTEXT_MENU_HOOKED(handle)
 
 end
 
+function REMOVE_CHAT_CLUSTER(clusterName, groupBoxName)
+	removedClusterNames[clusterName] = true;
+	local chatFrame = ui.GetFrame("chatframe");
+	if chatFrame == nil then
+		return;
+	end
+	local groupBox = GET_CHILD(chatFrame, groupBoxName, "ui::CGroupBox");
+	local chatCtrl = GET_CHILD(groupBox, clusterName);
+	
+	chatCtrl:Resize(0, 0);
+	chatCtrl:ShowWindow(0);
+	GBOX_AUTO_ALIGN(groupBox, 0, 0, 0, true, false);
+	groupBox:UpdateData();
 
+	chatFrame:Invalidate();
+end
+
+
+function RESIZE_CHAT_CTRL_HOOKED(chatCtrl, label, txt, timeBox)
+	if removedClusterNames[chatCtrl:GetName()] == true then 
+		local groupBox = chatCtrl:GetParent();
+		chatCtrl:Resize(1, 0);
+		chatCtrl:ShowWindow(0);
+		GBOX_AUTO_ALIGN(groupBox, 0, 0, 0, true, false);
+		groupBox:UpdateData();
+		return;
+	end
+	_G['RESIZE_CHAT_CTRL_OLD'](chatCtrl, label, txt, timeBox);
+end
+
+if not removedClusterNames then
+	removedClusterNames = {};
+end
+
+SETUP_HOOK(RESIZE_CHAT_CTRL_HOOKED, "RESIZE_CHAT_CTRL");
 SETUP_HOOK(SHOW_PC_CONTEXT_MENU_HOOKED, "SHOW_PC_CONTEXT_MENU");
 SETUP_HOOK(CHAT_RBTN_POPUP_HOOKED, "CHAT_RBTN_POPUP");
 SETUP_HOOK(OPEN_PARTY_MEMBER_INFO_HOOKED, "OPEN_PARTY_MEMBER_INFO");
